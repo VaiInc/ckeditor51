8ea03ef6655d550416d0c7bb92b0545cc124b0db
Hussnain Jutt <huxxnainali@gmail.com>
2023-11-22 23:57:35 +0000

    added new package

--- packages/ckeditor5-build-classic/src/ckeditor.ts
+++ packages/ckeditor5-build-classic/src/ckeditor.ts
@@ -9,13 +9,29 @@ import { ClassicEditor as ClassicEditorBase } from '@ckeditor/ckeditor5-editor-c
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
 import { UploadAdapter } from '@ckeditor/ckeditor5-adapter-ckfinder';
 import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import {
+	Bold,
+	Italic,
+	Strikethrough,
+	Code,
+	Subscript,
+	Superscript,
+} from '@ckeditor/ckeditor5-basic-styles';
 import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
 import { CKBox } from '@ckeditor/ckeditor5-ckbox';
 import { CKFinder } from '@ckeditor/ckeditor5-ckfinder';
 import { EasyImage } from '@ckeditor/ckeditor5-easy-image';
 import { Heading } from '@ckeditor/ckeditor5-heading';
-import { Image, ImageCaption, ImageStyle, ImageToolbar, ImageUpload, PictureEditing } from '@ckeditor/ckeditor5-image';
+import { Highlight } from '@ckeditor/ckeditor5-highlight';
+import { Font } from '@ckeditor/ckeditor5-font';
+import {
+	Image,
+	ImageCaption,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	PictureEditing,
+} from '@ckeditor/ckeditor5-image';
 import { Indent } from '@ckeditor/ckeditor5-indent';
 import { Link } from '@ckeditor/ckeditor5-link';
 import { List } from '@ckeditor/ckeditor5-list';
@@ -25,65 +41,107 @@ import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
 import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
 import { TextTransformation } from '@ckeditor/ckeditor5-typing';
 import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
+import { ImageInsert } from '@ckeditor/ckeditor5-image';
+import { AutoImage } from '@ckeditor/ckeditor5-image';
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed';
 
 export default class ClassicEditor extends ClassicEditorBase {
-	public static override builtinPlugins = [
-		Essentials,
-		UploadAdapter,
-		Autoformat,
-		Bold,
-		Italic,
-		BlockQuote,
-		CKBox,
-		CKFinder,
-		CloudServices,
-		EasyImage,
-		Heading,
-		Image,
-		ImageCaption,
-		ImageStyle,
-		ImageToolbar,
-		ImageUpload,
-		Indent,
-		Link,
-		List,
-		MediaEmbed,
-		Paragraph,
-		PasteFromOffice,
-		PictureEditing,
-		Table,
-		TableToolbar,
-		TextTransformation
-	];
-
-	public static override defaultConfig = {
-		toolbar: {
-			items: [
-				'undo', 'redo',
-				'|', 'heading',
-				'|', 'bold', 'italic',
-				'|', 'link', 'uploadImage', 'insertTable', 'blockQuote', 'mediaEmbed',
-				'|', 'bulletedList', 'numberedList', 'outdent', 'indent'
-			]
-		},
-		image: {
-			toolbar: [
-				'imageStyle:inline',
-				'imageStyle:block',
-				'imageStyle:side',
-				'|',
-				'toggleImageCaption',
-				'imageTextAlternative'
-			]
-		},
-		table: {
-			contentToolbar: [
-				'tableColumn',
-				'tableRow',
-				'mergeTableCells'
-			]
-		},
-		// This value must be kept in sync with the language defined in webpack.config.js.
-		language: 'en'
-	};
+	static editorConfig: (config: any) => void;
 }
+ClassicEditor.builtinPlugins = [
+	Essentials,
+	UploadAdapter,
+	Autoformat,
+	Bold,
+	Italic,
+	BlockQuote,
+	CKBox,
+	CKFinder,
+	CloudServices,
+	EasyImage,
+	Heading,
+	Image,
+	ImageCaption,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	Indent,
+	Link,
+	List,
+	MediaEmbed,
+	Paragraph,
+	PasteFromOffice,
+	PictureEditing,
+	Table,
+	TableToolbar,
+	TextTransformation,
+	HtmlEmbed,
+	Highlight,
+	Font,
+	ImageInsert,
+	AutoImage,
+	Strikethrough,
+	Code,
+	Subscript,
+	Superscript,
+	Alignment,
+];
+
+ClassicEditor.defaultConfig = {
+	alignment: {
+		options: ['center', 'left', 'right', 'justify'],
+	},
+	toolbar: {
+		items: [
+			'undo',
+			'redo',
+			'|',
+			'heading',
+			'|',
+			'bold',
+			'italic',
+			'fontSize',
+			'fontFamily',
+			'fontColor',
+			'fontBackgroundColor',
+			'strikethrough',
+			'code',
+			'highlight',
+			'alignment',
+			'|',
+			'link',
+			// 'uploadImage',
+			'insertImage',
+			'insertTable',
+			'blockQuote',
+			'mediaEmbed',
+			'|',
+			'bulletedList',
+			'numberedList',
+			'outdent',
+			'indent',
+
+			'htmlEmbed',
+		],
+	},
+	image: {
+		toolbar: [
+			'imageStyle:inline',
+			'imageStyle:block',
+			'imageStyle:side',
+			'|',
+			'toggleImageCaption',
+			'imageTextAlternative',
+		],
+	},
+	table: {
+		contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
+	},
+	// This value must be kept in sync with the language defined in webpack.config.js.
+	language: 'en',
+};
+
+ClassicEditor.editorConfig = function (config) {
+	config.contentsLangDirection = 'rtl';
+};
